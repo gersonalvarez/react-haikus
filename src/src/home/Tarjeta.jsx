d5d5ac154120bb1e8b880e78c05f6044e15564bd
JanOmonte <gersonalvarezetec@gmail.com>
2021-11-29 13:42:42 -0300

    Update Tarjeta.jsx

--- src/src/home/Tarjeta.jsx
+++ src/src/home/Tarjeta.jsx
@@ -4,33 +4,33 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 
-const Tarjeta = ({ imagen, fotoDePerfil, NombreDelHaiku, haiku}) => {
+const Tarjeta = ({ imagen, fotoDePerfil, NombreDelHaiku, haiku }) => {
   const [invisible, setInvisible] = useState(true);
 
 
 
   const cambiarVisibilidad = () => {
-    if( invisible === false  ) {
+    if (invisible === false) {
       setInvisible(true);
-    }else if( invisible === true ) {
+    } else if (invisible === true) {
       setInvisible(false);
     }
   }
 
 
-      return (
-  <div>
+  return (
+    <div>
       <div className="imagenYHaiku" onClick={cambiarVisibilidad}>
-      <p className="Haiku" style={{ display: invisible ? "none" : "block" }}> {haiku} </p>
-      
-      <motion.img src={imagen} alt="/" className="imagenP" whileTap={{ scale: 0.8 }}/>
+        <p className="Haiku" style={{ display: invisible ? "none" : "block" }}> {haiku} </p>
+
+        <motion.img src={imagen} alt="/" className="imagenP" whileTap={{ scale: 0.8 }} />
       </div>
       <div className="cuerpoTarjeta">
         <div className="fotoYTituloH">
           <img src={fotoDePerfil} alt="fotoDePerfil" className="fotoDePerfil" />
           <p className="TituloDelHaiku">{NombreDelHaiku}</p>
         </div>
-        <FontAwesomeIcon icon={faShareAlt} className="icono"/>
+        <FontAwesomeIcon icon={faShareAlt} className="icono" />
       </div>
     </div>
   );
